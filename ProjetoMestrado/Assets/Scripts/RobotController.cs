7c6819f6f2f0501679f5574e2beeff4302782620
Pedro <phbirais@gmail.com>
2020-10-10 22:19:55 -0300

    First commit
    das

---
+++ ProjetoMestrado/Assets/Scripts/RobotController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    public Rigidbody robotBody;
+
+    public GameObject marker;
+    public GameObject robot;
+
+    public float robotVelocity;
+    public Vector3 robotAngle;
+    public double distance, distanceY;
+
+    UI_Controller uiController;
+    
+    void Start()
+    {
+        uiController = GameObject.Find("UIManager").GetComponent<UI_Controller>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    Vector3 difference = (robot.transform.position - marker.transform.position);
+    float distanceX = Mathf.Abs(difference.x);
+    float distanceZ = Mathf.Abs(difference.z);
+    
+    //distance= Vector3.Distance (robot.transform.position, marker.transform.position);
+    //distance = Mathf.Round(distance * 100) / 100;
+    distanceX = Mathf.Round(distanceX * 100) / 100;
+    distanceZ = Mathf.Round(distanceZ * 100) / 100;
+
+    //print(distanceX);
+    distance = distanceX - 0.15;
+    distanceY = distanceZ;
+    robotVelocity = uiController.rightSliderValue;
+    robotBody.velocity = new Vector3(robotVelocity,0,0);
+
+    //mudar angulo do robô
+    //Quaternion deltaRotation = Quaternion.Euler((new Vector3(0, robotAngle, 0))* Time.deltaTime);
+    //robotAngle = new Vector3 (0, uiController.leftSliderValue, 0 );
+    //robot.transform.Rotate(robotAngle * Time.deltaTime);
+    //robotBody.MoveRotation(robotBody.rotation * deltaRotation);
+    
+    }
+}
---
+++ ProjetoMestrado/Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Controller : MonoBehaviour
+{
+    public Text sliderRigthValue;
+    public Text sliderLeftValue;
+    public Text distanceValueX, distanceValueY;
+    public Slider rightSlider;
+    public Slider leftSlider;
+
+    
+
+    public float rightSliderValue;
+
+    public float leftSliderValue;
+
+    // Start is called before the first frame update
+
+    RobotController robotController;
+    void Start()
+    {
+        robotController = GameObject.Find("RobotBody").GetComponent<RobotController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceValueX.text = robotController.distance.ToString();
+        distanceValueY.text = robotController.distanceY.ToString();
+    }
+
+     public void RightSliderValueChanged(float newValue)
+ {
+     rightSliderValue = Mathf.Round(rightSlider.value * 100) / 100;
+
+     sliderRigthValue.text = rightSliderValue.ToString();
+
+ }
+
+      public void LeftSliderValueChanged(float newValue)
+ {
+     leftSliderValue = Mathf.Round(leftSlider.value * 100) / 100;
+
+     sliderLeftValue.text = leftSliderValue.ToString();
+
+ }
+
+}

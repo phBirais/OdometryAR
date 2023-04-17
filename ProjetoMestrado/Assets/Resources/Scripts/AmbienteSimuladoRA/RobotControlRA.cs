1e530021896daebd39770f53e4b3e867a6310351
Pedro Henrique Birais <phbirais@gmail.com>
2023-04-17 18:48:02 -0300

    feat: Ui control RA simulation

---
+++ ProjetoMestrado/Assets/Resources/Scripts/AmbienteSimuladoRA/RobotControlRA.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotControlRA : MonoBehaviour
+{
+    public float wheelRadius = 0.017f; //Raio da roda em metros
+    public float bodyRadius = 0.05f; //Raio do corpo do robô em metros
+    public float leftWheelSpeed = 0.0f;
+    public float rightWheelSpeed = 0.0f;
+    public GameObject go;
+
+    //Input de texto
+    public InputField leftSpeedInput;
+    public InputField rightSpeedInput;
+
+    public float deltaT = 0.1f; //passo da operação
+
+    float cos_theta = 0, sin_theta = 0;
+
+    float theta = 0;
+
+    float xAtual, xAnterior, yAtual, yAnterior, anguloAtual, anguloAnterior = 0;
+
+    public float forwardSpeed = 0, angularSpeed = 0;
+    public double posX, posY, angle;
+
+    //status do app
+    public bool status = false;
+
+    Quaternion rotacaoInicial = Quaternion.identity;
+    void Start()
+    {
+        //InvokeRepeating("Main", 0.1f, deltaT);
+    }
+
+    public void StartRobot()
+    {
+        rotacaoInicial = go.transform.rotation;
+        if (int.TryParse(leftSpeedInput.text, out int result))
+        {
+            rightWheelSpeed = result;
+        }
+
+        if (int.TryParse(rightSpeedInput.text, out int result2))
+        {
+            leftWheelSpeed = result2;
+        }
+
+        leftWheelSpeed = ((float)(leftWheelSpeed / 9.5492965964254));
+        rightWheelSpeed = ((float)(rightWheelSpeed / 9.5492965964254));
+
+        status = true;
+
+        InvokeRepeating("Main", 0.1f, deltaT);
+    }
+
+    public void StopRobot()
+    {
+        CancelInvoke();
+        status = false;
+    }
+
+
+    public void ResetRobot()
+    {
+        go.transform.position = new Vector3(-0.15f, 0, 0);
+        go.transform.rotation = rotacaoInicial;
+        //go.transform.Rotate(0, -90, 0);
+        xAnterior = -0.15f;
+        yAnterior = 0;
+        xAtual = 0;
+        yAtual = 0;
+        cos_theta = 0;
+        sin_theta = 0;
+        theta = 0;
+        anguloAnterior = 0;
+    }
+
+    void Main()
+    {
+        if (status == true)
+        {
+            float v = -1 * FowardSpeed();
+            float omega = GetOmega();
+            xAtual = xAnterior + (v * cos_theta * deltaT);
+            yAtual = yAnterior + (v * sin_theta * deltaT);
+            cos_theta = Mathf.Cos(theta);
+            sin_theta = Mathf.Sin(theta);
+
+            xAnterior = xAtual;
+            yAnterior = yAtual;
+            anguloAnterior = theta;
+
+            //movimentacao
+            go.transform.position = new Vector3(xAtual, 0, yAtual);
+            go.transform.Rotate(0, (-(omega * deltaT) * Mathf.Rad2Deg), 0);
+
+            theta = anguloAnterior + (omega * deltaT);
+
+            forwardSpeed = v;
+            angularSpeed = omega;
+            //informações
+            posX = xAtual;
+            posY = yAtual;
+            angle = theta * Mathf.Rad2Deg;
+        }
+    }
+
+    float FowardSpeed()
+    {
+        float speed = 0;
+        speed = ((leftWheelSpeed * wheelRadius) / 2) + ((rightWheelSpeed * wheelRadius) / 2);
+        return speed;
+    }
+
+    float GetOmega()
+    {
+        float t = ((leftWheelSpeed * wheelRadius) - (rightWheelSpeed * wheelRadius)) / (2 * bodyRadius);
+
+        return t;
+    }
+
+    float rpm()
+    {
+        float rpm = (((9.55f) * rightWheelSpeed) + (leftWheelSpeed * 9.55f));
+        rpm /= 2;
+
+        return rpm;
+    }
+}
---
+++ ProjetoMestrado/Assets/Resources/Scripts/AmbienteSimuladoRA/SimulatedKalmanRA.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+
+public class SimulatedKalmanRA : MonoBehaviour
+{
+
+    public GameObject simulatedVirtualBot;
+    public GameObject kalmanBot;
+    public GameObject eCamBot;
+    public GameObject eOdonBot;
+
+    //outros scripts
+    public RobotControlRA robotcontrolRA;
+
+    float deltaT = 0.1f; //passo da operação
+
+    //estado estimado kalman anterior
+    public Vector<double> eKant = Vector<double>.Build.Dense(3, 0);
+
+    double[] ekInicial = { 1.0, 1.0, 1.0 }; //só para preencher o vetor
+    public Vector<double> eK;
+
+    //covariancia estado estimado pelo kalman anterior
+    Matrix<double> pKant = Matrix<double>.Build.DenseDiagonal(3, 3, 1.0f);
+    public Matrix<double> pK = Matrix<double>.Build.DenseDiagonal(3, 3, 0.25f);
+
+    // estado de inovação
+    public Vector<double> yK = Vector<double>.Build.Dense(3, 0);
+
+    //ganho de kalman
+    public Matrix<double> Kn = Matrix<double>.Build.DenseDiagonal(3, 3, 0);
+
+    //estado odometria x,y,teta da odometria; x, y e angulo
+    //colocar a odometria do virtual como teste
+    //depois calcular a odemtria do real com base na velocidade das rodas
+    public Vector<double> eOdon = Vector<double>.Build.Dense(3, 0);
+
+    //estado recebido da camera -> que vem do tracking vuforia x y e angulo
+    public Vector<double> eCam = Vector<double>.Build.Dense(3, 0);
+
+    //estado estimado a priori//
+    Vector<double> ePrior = Vector<double>.Build.Dense(3, 0);
+
+    //covariancia do estado a priori
+    Matrix<double> pkPrior = Matrix<double>.Build.DenseDiagonal(3, 3, 0);
+
+    Matrix<double> S = Matrix<double>.Build.DenseDiagonal(3, 3, 0);
+
+    //Matriz A é a matriz de transferencia do sistema(do passo anteiror ao seguinte)
+    //na fusão sensorial ela vem da odometria//Matriz A é identidade
+    Matrix<double> A = Matrix<double>.Build.DenseDiagonal(3, 3, 1.0);
+
+    //Matriz H - matriz de observabilidade do sistema
+    Matrix<double> H = Matrix<double>.Build.DenseDiagonal(3, 3, 1.0);
+
+    Matrix<double> Q = Matrix<double>.Build.DenseDiagonal(3, 3, 1.85);
+
+    Matrix<double> R = Matrix<double>.Build.DenseDiagonal(3, 3, 1.0);
+
+    //matriz identidade
+    Matrix<double> I = Matrix<double>.Build.DenseDiagonal(3, 3, 1.0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        eK = Vector<double>.Build.DenseOfArray(ekInicial);
+    }
+
+    public void StartKalman()
+    {
+        deltaT = robotcontrolRA.deltaT;
+        InvokeRepeating("CalculateKalmanFUsion", 0.1f, deltaT);
+
+    }
+
+    public void StopKalman()
+    {
+        CancelInvoke();
+    }
+
+    void CalculateKalmanFUsion()
+    {
+        if (robotcontrolRA.status == true)
+        {
+            //1 e 2)
+            eKant = eK;
+            pKant = pK;
+            //3)
+            eOdon[0] = eKant[0] + robotcontrolRA.forwardSpeed * Mathf.Cos((float)eKant[2]) * deltaT + Random.Range(0.005f, 0.03f);
+            eOdon[1] = eKant[1] + robotcontrolRA.forwardSpeed * Mathf.Sin((float)eKant[2]) * deltaT + Random.Range(0.005f, 0.03f);
+            eOdon[2] = eKant[2] + robotcontrolRA.angularSpeed * deltaT;
+
+            //4)
+            eCam[0] = simulatedVirtualBot.transform.position.x + Random.Range(0.00f, -0.01f); //x da camera
+            eCam[1] = simulatedVirtualBot.transform.position.z + Random.Range(0.00f, -0.01f);  //y da camera
+            eCam[2] = simulatedVirtualBot.transform.eulerAngles.y * Mathf.Deg2Rad; //teta da camera (em radianos)
+
+            //5)
+            ePrior = eOdon;
+
+            //6)
+            pkPrior = A * pKant * A.Transpose() + Q;
+
+            //7)
+            yK = eCam - (H * ePrior);
+
+            //8)
+            S = H * pkPrior * H.Transpose() + R;
+            //S = H * pKant * H.Transpose() + R;
+
+            //9)
+            Kn = pkPrior * H.Transpose() * S.Inverse();
+
+            //10)
+            eK = eOdon + (Kn * yK);
+
+            //11)
+            pK = (I - Kn * H) * pkPrior;
+            //pK = (I - Kk * H) * pkPrior * ((I - Kk * H).Transpose()) + (Kk * R * Kk.Transpose());
+
+            MoveRobot(eK, kalmanBot);
+            MoveRobot(eCam, eCamBot);
+            MoveRobot(eOdon, eOdonBot);
+
+            //Debug.Log("eCam = " + eCam);
+            //Debug.Log("eOdon = " + eOdon);
+            //Debug.Log("KK = " + Kn);
+            //Debug.Log("S = " + S);
+            //Debug.Log("Ek = " + eK);
+        }
+    }
+
+    public void MoveRobot(Vector<double> state, GameObject go)
+    {
+        go.transform.position = new Vector3((float)state[0], 0, (float)state[1]);
+        go.transform.rotation = Quaternion.Euler(0, (float)state[2] * Mathf.Rad2Deg, 0);
+    }
+}
